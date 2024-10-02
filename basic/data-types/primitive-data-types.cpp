3e645af2eefc4b8bc8d85a9545d2d41b226eaa3f
Shareef <shareef@kmzee.online>
2024-10-03 00:08:02 +0530

    Add Primitive data types

---
+++ basic/data-types/primitive-data-types.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int a = 10;
+  char b = 'a';
+  bool c = true;
+  float d = 20.5;
+  double e = 30.5;
+  long f = 40;
+  short g = 50;
+  unsigned int h = 60;
+  unsigned long i = 70;
+  unsigned short j = 80;
+  long long k = 90;
+  unsigned long long l = 100;
+
+  cout << "Integer(int): a = " << a << endl;
+  cout << "Character(char): b = " << b << endl;
+  cout << "Boolean(bool): c = " << c << endl;
+  cout << "Float(float): d = " << d << endl;
+  cout << "Double(double): e = " << e << endl;
+  cout << "Long(long): f = " << f << endl;
+  cout << "Short(short): g = " << g << endl;
+  cout << "Unsigned Integer(unsigned int): h = " << h << endl;
+  cout << "Unsigned Long(unsigned long): i = " << i << endl;
+  cout << "Unsigned Short(unsigned short): j = " << j << endl;
+  cout << "Long Long(long long): k = " << k << endl;
+  cout << "Unsigned Long Long(unsigned long long): l = " << l << endl;
+
+  return 0;
+}
---
+++ basic/data-types/readme.md
@@ -0,0 +1,10 @@
+# Data Types in C++
+
+| Data Type             | Keyword | Bid Width | Range                                                 |
+| --------------------- | ------- | --------- | ----------------------------------------------------- |
+| Boolean               | bool    | -         | -                                                     |
+| Character             | char    | 1 byte    | -127 to 127 or 0-255                                  |
+| Integer               | int     | 4 bytes   | -2,147,483,648 to 2,147,483,647 or 0 to 4,294,967,295 |
+| Floating Point        | float   | 4 bytes   | -3.4E38 to 3.4E38                                     |
+| Double floating point | double  | 8 bytes   | -1.7E308 to 1.7E308                                   |
+| Void                  | void    | -         | -                                                     |
